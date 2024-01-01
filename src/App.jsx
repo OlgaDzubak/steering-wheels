17c8d48fe5058f842c3d04b9f72eb8c93e07bb10
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-01-02 00:06:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
               <Header/>
 
               <Routes>
-                <Route  path="/"  element={ <Route redirectTo="/" component={ <Main/>} /> } />
+                <Route  path=""  element={ <Route redirectTo="" component={ <Main/>} /> } />
               </Routes>
 
               <Footer/>
