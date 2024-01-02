443c55057848bfe941cbb244914b26c7bb1d0ece
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-01-02 03:38:53 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
               <Routes>
               
                 <Route 
-                  path="/steering-wheels" 
+                  path="/" 
                   element={ <Main/> }
                 />
 
