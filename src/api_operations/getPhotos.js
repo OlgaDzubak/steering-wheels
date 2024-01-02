f176534b9dd3144990abfd755257ef25f8fee1d7
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-01-02 03:31:36 +0200

    Update getPhotos.js

--- src/api_operations/getPhotos.js
+++ src/api_operations/getPhotos.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 async function getPhotos(page, per_page) {
       axios.defaults.params;
       console.log("я в getPhotos");
-      const data = await axios.get(`steering-wheels/query?page=${page}&per_page=${per_page}`);
+      const data = await axios.get(`/query?page=${page}&per_page=${per_page}`);
       return data;
 }
 
