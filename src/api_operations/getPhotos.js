f8deaf7152881b374bf4b551f140841476e88b64
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-01-02 05:43:47 +0200

    Update getPhotos.js

--- src/api_operations/getPhotos.js
+++ src/api_operations/getPhotos.js
@@ -5,7 +5,7 @@ axios.defaults.baseURL = 'https://steering-wheels-backend.onrender.com';
 async function getPhotos(page, per_page) {
       
       console.log("Я в getPhotos");
-      const data = await axios.get(`search?page=${page}&per_page=${per_page}`);
+      const data = await axios.get(`/search?page=${page}&per_page=${per_page}`);
       return data;
 }
 
