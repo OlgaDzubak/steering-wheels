844a4f678f4ca79adfcb1318d1ed292f2efdd2e1
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-01-09 17:21:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   
   return  <Routes>
             <Route  path ="/" element={ <HomePage />  }/>
-            <Route  path ="#ua" element={ <HomePage />  }/>
             <Route  path="*"  element={ <ErrorPage /> } />
           </Routes>
 };
