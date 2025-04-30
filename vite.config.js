9ee907839bc4c4c20c4af5d56e0d6165c4c79de8
Olga Dzubak <iamAndriy80@gmail.com>
2025-04-30 09:58:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from 'vite-plugin-svgr';
-import { analyzer } from 'vite-bundle-analyzer';
 
 export default defineConfig({
   server: {
     open: true, // Автоматически открывать браузер при запуске сервера
   },
-  plugins: [react(), svgr(), analyzer()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       src: '/src',
