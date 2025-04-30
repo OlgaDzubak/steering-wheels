7172d6ef76f75e4577028d1b836abe612f0be5f5
Olga Dzubak <iamAndriy80@gmail.com>
2025-04-30 12:15:04 +0300

    Update GalleryItem.jsx

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,7 +4,7 @@ import { Item_li, Item_img, Item_P } from "./GalleryItem.styled";
 const GalleryItem = ({url, alt, onGalleryItemClick}) => {
     
     return  <Item_li onClick={onGalleryItemClick}>
-                    <Item_img src={url} alt={alt} width="400" />
+                    <Item_img src={url} alt={alt} width="400" height="auto"/>
                     <Item_P>{alt}</Item_P>
                 </Item_li>
 };
